d655a36f71403c3f21d05bf1e4f2ac2e4887ace0
jbeuler <jbeuler93@gmail.com>
2022-03-18 17:02:33 -0400

    Models definition

---
+++ Gridiron.HomeTest/Models/Application.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gridiron.HomeTest.Models
+{
+    public class Application
+    {
+        public Application()
+        {
+        }
+
+        [Key]
+        public int ApplicationId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        // mm/dd/yy
+        [Column(TypeName = "nvarchar(8)")]
+        public string EffectiveDate { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string AddressStreet { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string AddressCity { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string AddressState { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string AddressZip { get; set; }
+
+        [Column(TypeName = "nvarchar(15)")]
+        public string InsuredValueAmount { get; set; }
+
+
+    }
+}
---
+++ Gridiron.HomeTest/Models/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Gridiron.HomeTest.Models
+{
+    public class ApplicationContext
+    {
+        public ApplicationContext()
+        {
+        }
+    }
+}
